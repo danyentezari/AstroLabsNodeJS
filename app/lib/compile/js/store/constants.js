a35528f7590b6c7bbe4a028d0fb8b948d64a1b97
Dany Entezari <dany.entezari@gmail.com>
2017-10-26 11:21:48 +0400

    Replaced app files.

--- app/lib/compile/js/store/constants.js
+++
@@ -1,8 +0,0 @@
-const constants = {
-	GET_CARDS: "GET_CARDS",
-	SET_CARDS: "SET_CARDS",
-	CHANGE_CARD: "CHANGE_CARD",
-	EDIT_CARD: "EDIT_CARD",
-	DELETE_CARD: "DELETE_CARD"
-}
-export default constants
